2c1c8c2aee4084d9eb5f66f57be682c8f987a6b7
1313ou <1313ou@gmail.com>
2026-08-18 08:32:23 +0200

    cli

--- src/main/java/org/oewntk/json/main.kt
+++ src/main/java/org/oewntk/json/main.kt
@@ -6,6 +6,11 @@ import com.networknt.schema.SchemaRegistry
 import com.networknt.schema.SchemaLocation
 import com.networknt.schema.SpecificationVersion
 import com.networknt.schema.InputFormat
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.vararg
+import kotlin.system.exitProcess
 
 class Validator(schema: String) {
 
@@ -33,27 +38,44 @@ private fun toSchema(selector: String): String {
 
 fun main(args: Array<String>) {
 
-    val validator = Validator(toSchema(args[0]))
-    println("Schema ${validator.schema.id}")
-    args.asSequence()
-        .drop(1)
-        .forEach { f ->
-            // Data node
-            val jsonString = File(f).readText()
+    val parser = ArgParser("validator")
+    // Options (start with - or --)
+    // @formatter:off
+    val schema by parser.argument(          ArgType.String,                                                      description = "Schema")
+    val inputs by parser.argument(          ArgType.String,                                                      description = "File inputs")    .vararg()
+    val verbose by parser.option(           ArgType.Boolean,       shortName = "v",  fullName = "verbose",       description = "Verbose output") .default(false)
 
-            // Validate
-            val errors = validator.validate(jsonString)
+    val traceTime by parser.option(         ArgType.Boolean,       shortName = "tt", fullName = "trace:time",    description = "trace time")     .default(false)
+    val traceHeap by parser.option(         ArgType.Boolean,       shortName = "th", fullName = "trace:heap",    description = "trace heap")     .default(false)
+    // @formatter:on
 
-            // Result
-            if (errors.isEmpty()) {
-                println("$f is valid!")
-            } else {
-                println("❌ Validation of $f failed with ${errors.size} error(s):")
-                errors
-                    .take(3)
-                    .forEach { error ->
-                        println("  - Path: ${error.property} | Message: ${error.message} | Instance: ${error.instanceNode}")
-                    }
-            }
+    parser.parse(args)
+    if (verbose) {
+        System.err.println("Schema: $schema")
+        System.err.println("Files: $inputs")
+    }
+    val validator = Validator(toSchema(schema))
+    if (verbose) {
+        System.err.println("Schema id: ${validator.schema.id}")
+    }
+    inputs.forEach { f ->
+        // Data node
+        val jsonString = File(f).readText()
+
+        // Validate
+        val errors = validator.validate(jsonString)
+
+        // Result
+        if (errors.isEmpty()) {
+            println("-$f is valid")
+        } else {
+            System.err.println("❌ Validation of $f failed with ${errors.size} error(s):")
+            errors
+                .take(3)
+                .forEach { error ->
+                    System.err.println("  - Path: ${error.property} | Message: ${error.message} | Instance: ${error.instanceNode}")
+                }
+            exitProcess(1)
         }
+    }
 }
